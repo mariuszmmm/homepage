fe335280d3b8e6b2f1db30c43927436465774757
Mariusz Matusiewicz <mariuszmmm@op.pl>
2022-11-07 20:01:43 +0100

    function improvement

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 {
-    const langPl = document.querySelector(".section__langPl")
-    const langEng = document.querySelector(".section__langEng")
-    const colorButtonTextEng = document.querySelector(".section__buttonTextEng");
-    const colorButtonTextPl = document.querySelector(".section__buttonTextPl");
-
-    const defaultSettings = () => {
-        langPl.classList.add("section__langPl");
+    const defaultSettings = (langPl, langEng, colorButtonTextPl, colorButtonTextEng) => {
+        langPl.classList.add("js-langPl");
         langEng.classList.remove("section__langEng");
-        colorButtonTextEng.classList.add("section__buttonTextEng")
         colorButtonTextPl.classList.remove("section__buttonTextPl")
+        colorButtonTextEng.classList.add("section__buttonTextEng")
     }
-
-    const clickChangeLanguage = () => {
-        langPl.classList.toggle("section__langPl");
-        langEng.classList.toggle("section__langEng");
-        colorButtonTextEng.classList.toggle("section__buttonTextEng")
-        colorButtonTextPl.classList.toggle("section__buttonTextPl")
-    }
-
     const init = () => {
         const buttonElement = document.querySelector(".js-button");
-        defaultSettings();
-        buttonElement.addEventListener("click", clickChangeLanguage)
+        const langPl = document.querySelector(".js-langPl")
+        const langEng = document.querySelector(".js-langEng")
+        const colorButtonTextEng = document.querySelector(".js-buttonTextEng");
+        const colorButtonTextPl = document.querySelector(".js-buttonTextPl");
+        defaultSettings(langPl, langEng, colorButtonTextPl, colorButtonTextEng);
+        buttonElement.addEventListener("click", () => {
+            langPl.classList.toggle("section__langPl");
+            langEng.classList.toggle("section__langEng")
+            colorButtonTextEng.classList.toggle("section__buttonTextEng")
+            colorButtonTextPl.classList.toggle("section__buttonTextPl")
+        })
     }
     init();
 }
