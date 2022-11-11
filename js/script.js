65beacb567e1cc548ea9098c7ce4c985c347a678
Mariusz Matusiewicz <mariuszmmm@op.pl>
2022-11-11 01:23:09 +0100

    change in js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
 {
-    const defaultSettings = (langPl, langEng, colorButtonTextPl, colorButtonTextEng) => {
-        langPl.classList.add("js-langPl");
-        langEng.classList.remove("section__langEng");
-        colorButtonTextPl.classList.remove("section__buttonTextPl")
-        colorButtonTextEng.classList.add("section__buttonTextEng")
-    }
     const init = () => {
         const buttonElement = document.querySelector(".js-button");
         const langPl = document.querySelector(".js-langPl")
         const langEng = document.querySelector(".js-langEng")
         const colorButtonTextEng = document.querySelector(".js-buttonTextEng");
         const colorButtonTextPl = document.querySelector(".js-buttonTextPl");
-        defaultSettings(langPl, langEng, colorButtonTextPl, colorButtonTextEng);
         buttonElement.addEventListener("click", () => {
-            langPl.classList.toggle("section__langPl");
-            langEng.classList.toggle("section__langEng")
-            colorButtonTextEng.classList.toggle("section__buttonTextEng")
-            colorButtonTextPl.classList.toggle("section__buttonTextPl")
+            langPl.classList.toggle("section__hidden");
+            langEng.classList.toggle("section__hidden")
+            colorButtonTextEng.classList.toggle("section__colorActivLang")
+            colorButtonTextPl.classList.toggle("section__colorActivLang")
         })
     }
     init();
